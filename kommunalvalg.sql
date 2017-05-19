b29710a1163a0dfdcb54a9f474c657e8a3814cd5
Slepzs <slepzs@gmail.com>
2017-05-19 11:05:33 +0200

    Databasen
    Inkludere: Kommentarer og politiker navn + parti

---
+++ kommunalvalg.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.5
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 19, 2017 at 08:46 AM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kommunalvalg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) NOT NULL COMMENT 'primary key',
+  `comment` text NOT NULL COMMENT 'comment'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `politicians`
+--
+
+CREATE TABLE IF NOT EXISTS `politicians` (
+  `id` int(11) NOT NULL COMMENT 'Primary key',
+  `polname` varchar(255) NOT NULL COMMENT 'politician name',
+  `party` varchar(255) NOT NULL COMMENT 'party'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `politicians`
+--
+ALTER TABLE `politicians`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key';
+--
+-- AUTO_INCREMENT for table `politicians`
+--
+ALTER TABLE `politicians`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary key';
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
