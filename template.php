d3c82d72033eeaaa10deeaea7df37b4ce4aa81cf
Slepzs <slepzs@gmail.com>
2017-05-22 16:26:16 +0200

    Template + events side

---
+++ template.php
@@ -0,0 +1,24 @@
+<?php include 'include/analytics.php'; ?>
+<?php require_once 'include/cookie.php'; ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include 'include/head.php'; ?>
+    <title>Seneste Nyt</title>
+  </head>
+<body>
+<?php include 'include/nav.php' ?>
+<div class="sectionhero_1">
+
+</div>
+
+<div class="sectionhero_1">
+
+</div>
+
+
+  <?php include 'include/footer.php'; ?>
+  <?php include 'include/jquary.php'; ?>
+  </body>
+</html>
