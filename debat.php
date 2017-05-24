55267922788caf7f7b154528a576c7928c86434a
CamillaVoss <camilla@5oss.dk>
2017-05-24 12:59:59 +0200

    Styling af debatside og tilføjelse af ikoner

--- debat.php
+++ debat.php
@@ -41,6 +41,73 @@
 	</div>
 </div>
 
+<div class="debat">
+	<h1> EL mener at der er for få hænder i børnehaven - hvad mener du? </h1>
+	<div class="debat-post">
+		<div class="debat-post-content">
+			<h2> Stakkels dem!</h2>
+			<p>Jeg er enig!
+				Puha, det må være hårdt at være pædagog i vore dage. Aldrig har der været så lidt personale til så mange børn.
+			</p>
+		</div>
+		<div class="debat-post-points">
+			<div class="debat-post-points-up">
+				<img src="images/ikoner/greenArrow.svg">
+				<h3>7</h3>
+			</div>
+			<div class="debat-post-points-down">
+				<img src="images/ikoner/redArrow.svg">
+				<h3>1</h3>
+			</div>
+		</div>
+	</div>
+	<div class="debat-post">
+		<div class="debat-post-content">
+			<h2> Stakkels dem!</h2>
+			<p>Jeg er enig!
+				Puha, det må være hårdt at være pædagog i vore dage. Aldrig har der været så lidt personale til så mange børn.
+			</p>
+		</div>
+		<div class="debat-post-points">
+			<div class="debat-post-points-up">
+				<img src="images/ikoner/greenArrow.svg">
+				<h3>7</h3>
+			</div>
+			<div class="debat-post-points-down">
+				<img src="images/ikoner/redArrow.svg">
+				<h3>1</h3>
+			</div>
+		</div>
+	</div>
+	<div class="debat-post">
+		<div class="debat-post-content">
+			<h2> Stakkels dem!</h2>
+			<p>Jeg er enig!
+				Puha, det må være hårdt at være pædagog i vore dage. Aldrig har der været så lidt personale til så mange børn.
+			</p>
+		</div>
+		<div class="debat-post-points">
+			<div class="debat-post-points-up">
+				<img src="images/ikoner/greenArrow.svg">
+				<h3>7</h3>
+			</div>
+			<div class="debat-post-points-down">
+				<img src="images/ikoner/redArrow.svg">
+				<h3>1</h3>
+			</div>
+		</div>
+	</div>
+	<div class="debatform">
+		<form>
+		    <input type="text" id="headline" name="headline" placeholder="Overskrift" class="input">
+
+		    <textarea id="kommentar" name="kommentar" placeholder="Skriv din kommentar" class="input textbox"></textarea>
+		    <br>
+		    <input type="submit" value="Send" class="button">
+		</form>
+	</div>
+</div>
+
 
 
   <?php include 'include/footer.php'; ?>
