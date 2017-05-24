97f9c3607621e64fdd2699e8099316f39d154d73
CamillaVoss <camilla@5oss.dk>
2017-05-24 11:45:35 +0200

    Begyndende debatside og ikoner dertil

---
+++ debat.php
@@ -0,0 +1,49 @@
+<?php include 'include/analytics.php'; ?>
+<?php require_once 'include/cookie.php'; ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include 'include/head.php'; ?>
+    <title>Seneste Nyt</title>
+  </head>
+<body>
+<?php include 'include/nav.php' ?>
+
+<div class="debathero">
+	<div class="debathero-img">
+		<img src="images/mening1x.png">
+	</div>
+	<div class="debathero-vote">
+		<h2>EL mener at der er for få hænder i børnehaven - hvad mener du?</h2>
+		<table>
+		  <tr>
+		    <td><img src="images/ikoner/A.svg"></td>
+		    <td>Meget enig</td>
+		  </tr>
+		  <tr>
+		    <td><img src="images/ikoner/B.svg"></td>
+		    <td>Enig</td>
+		  </tr>
+		  <tr>
+		    <td><img src="images/ikoner/C.svg"></td>
+		    <td>Ved ikke</td>
+		  </tr>
+		  <tr>
+		    <td><img src="images/ikoner/D.svg"></td>
+		    <td>Uenig</td>
+		  </tr>
+		  <tr>
+		    <td><img src="images/ikoner/E.svg"></td>
+		    <td>Meget uenig</td>
+		  </tr>
+		</table>
+	</div>
+</div>
+
+
+
+  <?php include 'include/footer.php'; ?>
+  <?php include 'include/jquary.php'; ?>
+  </body>
+</html>
