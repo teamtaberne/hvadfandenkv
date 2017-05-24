91524f88247a643030ed4179948540f7c003fe45
Slepzs <slepzs@gmail.com>
2017-05-24 10:04:12 +0200

    Fixes

--- stalk.php
+++ stalk.php
@@ -9,13 +9,37 @@
   </head>
 <body>
 <?php include 'include/nav.php' ?>
-<div class="sectionhero_1">
-
+<div class="search_1">
+  <div class="col-1-1 mobile-col-1-1">
+    <input type="search" name="Search" value="SØG">
+  </div>
 </div>
-
-<div class="sectionhero_1">
-
-</div>
+  <div class="sectionhero_2">
+    <div class="grid grid-pad pollogos">
+      <div class="col-1-2 mobile-col-1-1 pollogo">
+        <a href="venstre.php"><img src="images/pollogos/venstre.jpg" alt="Venstre logo"></a>
+      </div>
+      <div class="col-1-2 mobile-col-1-1 pollogo">
+        <a href="venstre.php"><img src="images/pollogos/alter.png" alt="Venstre logo"></a>
+      </div>
+      <div class="col-1-2 mobile-col-1-1 pollogo">
+        <a href="venstre.php"><img src="images/pollogos/SF.jpg" alt="Venstre logo"></a>
+      </div>
+      <div class="col-1-2 mobile-col-1-1 pollogo">
+        <a href="venstre.php"><img src="images/pollogos/Ø.jpg" alt="Venstre logo"></a>
+      </div>
+      <div class="col-1-2 mobile-col-1-1 pollogo">
+        <a href="venstre.php"><img src="images/pollogos/LA.jpg" alt="Venstre logo"></a>
+      </div>
+      <div class="col-1-2 mobile-col-1-1 pollogo">
+        <a href="venstre.php"><img src="images/pollogos/DF.jpg" alt="Venstre logo"></a>
+    </div>
+    </div>
+  </div>
+  <div class="sectionhero_2">
+    <div class="grid grid-pad pollogos_2">
+  </div>
+  </div>
 
 
   <?php include 'include/footer.php'; ?>
---
+++ venstre.php
@@ -0,0 +1,74 @@
+<?php include 'include/analytics.php'; ?>
+<?php require_once 'include/cookie.php'; ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include 'include/head.php'; ?>
+    <title>Seneste Nyt</title>
+  </head>
+<body>
+<?php include 'include/nav.php' ?>
+<div class="sectionhero_2_venstre">
+<div class="grid grid-pad venstre">
+  <div class="col-1-1 mobile-col-1-1">
+    <h1>VENSTRE</h1>
+  </div>
+  <div class="col-1-1 markesager">
+    <h1>MÆRKESAGER</h1>
+  </div>
+</div>
+<div class="grid grid-pad venstre_1">
+  <div class="col-1-2 icons">
+    <ul>
+      <li><img src="images/ikoner/ungdom.png" alt="Ungdom"> Su'en skal ned!</li>
+      <li><img src="images/ikoner/beskæftigelse.png" alt="beskæftigelse"> Fuck kontantloftet</li>
+      <li><img src="images/ikoner/miljø.png" alt="Miljø"> Hulk har også følelser</li>
+    </ul>
+  </div>
+  <div class="col-1-2 icons">
+    <ul>
+      <li><img src="images/ikoner/sundhed2.png" alt=""> Hospitaler er for de svage</li>
+      <li><img src="images/ikoner/økonomi.png" alt=""> Skat er til mænd</li>
+      <li><img src="images/ikoner/kultur.png" alt=""> Gratis tv</li>
+    </ul>
+    </div>
+</div>
+</div>
+<div class="polpic">
+<div class="col-1-2 photo">
+  <a href="larslykke.php"><img src="images/larslykke.jpg" alt=""></a>
+  <h1>Lars Lykke</h1>
+</div>
+  <div class="col-1-2 photo">
+    <a href="larslykke.php"><img src="images/larslykke.jpg" alt=""></a>
+    <h1>Lars Lykke</h1>
+  </div>
+</div>
+<div class="polpic_2">
+  <div class="col-1-2 photo_1">
+    <a href="larslykke.php"><img src="images/larslykke.jpg" alt=""></a>
+    <h1>Lars Lykke</h1>
+  </div>
+  <div class="col-1-2 photo_1">
+    <a href="larslykke.php"><img src="images/larslykke.jpg" alt=""></a>
+    <h1>Lars Lykke</h1>
+  </div>
+</div>
+<div class="polpic">
+  <div class="col-1-2 photo">
+    <a href="larslykke.php"><img src="images/larslykke.jpg" alt=""></a>
+    <h1>Lars Lykke</h1>
+  </div>
+  <div class="col-1-2 photo">
+      <a href="larslykke.php"><img src="images/larslykke.jpg" alt=""></a>
+      <h1>Lars Lykke</h1>
+  </div>
+</div>
+
+
+
+  <?php include 'include/footer.php'; ?>
+  <?php include 'include/jquary.php'; ?>
+  </body>
+</html>
