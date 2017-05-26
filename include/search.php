e6bd5a0fe55fe71c6f72584f4ee16b66ce5c75d7
Camilla Voss <camilla@5oss.dk>
2017-05-26 16:57:03 +0200

    Søgebar stylet

--- include/search.php
+++ include/search.php
@@ -1,14 +1,12 @@
-<div class="search_1">
-    <div class="col-1-1 mobile-col-1-1">
-      <form name="search" method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
-          <div class="col-10-12">
-            Søg:  <input type="text" name="search"/>
-          </div>
-          <div class="col-2-12">
-            <input type="submit" value="Search" />
-          </div>
-        </form>
-    </div>
+<div class="search">
+    <form name="search" method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
+        <div class="search-input">
+          <input type="text" name="search"/>
+        </div>
+        <div class="search-btn">
+          <input type="submit" value="Search" />
+        </div>
+      </form>
 </div>
 
 <?php echo $output; ?>
