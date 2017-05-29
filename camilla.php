68f809928e183f74a2f87c12f95e4fa9fcdc5035
Slepzs <slepzs@gmail.com>
2017-05-29 19:13:40 +0200

    ??

--- camilla.php
+++ camilla.php
@@ -53,15 +53,15 @@
           <div class="hero-content-facts-left">
             <table>
               <tr>
-                <td><img src="images/ikoner/alder.png" alt="Kommunalvalg - Alder"></td>
+                <td><img src="images/ikoner/alder.png" alt="Kommunalvalg - Alder" title="Alder"></td>
                 <td>22 år</td>
               </tr>
               <tr>
-                <td><img src="images/ikoner/hjemby.png" alt="Kommunalvalg - Hjemby"></td>
+                <td><img src="images/ikoner/hjemby.png" alt="Kommunalvalg - Hjemby" title="Hjemby"></td>
                 <td>Helsingør</td>
               </tr>
               <tr>
-                <td><img src="images/ikoner/kommune.png" alt="Kommunalvalg - Kommune"></td>
+                <td><img src="images/ikoner/kommune.png" alt="Kommunalvalg - Kommune" title="Kommune"></td>
                 <td>Gentofte Kommune</td>
               </tr>
             </table>
@@ -69,15 +69,15 @@
           <div class="hero-content-facts-right">
             <table>
               <tr>
-                <td><img src="images/ikoner/sparetime.png" alt="Kommunalvalg - Fritid"></td>
+                <td><img src="images/ikoner/sparetime.png" alt="Kommunalvalg - Fritid" title="Fritid"></td>
                 <td>Jeg leger med Hugo hunden</td>
               </tr>
               <tr>
-                <td><img src="images/ikoner/funfact.png" alt="Kommunalvalg - Funfact"></td>
+                <td><img src="images/ikoner/funfact.png" alt="Kommunalvalg - Funfact" title="Funfact"></td>
                 <td>Camilla elsker Ikea på lige fod med Simpson, og er et kæmpe hyggedyr til hverdag</td>
               </tr>
               <tr>
-                <td><img src="images/ikoner/citat.png" alt="Kommunalvalg - Citat"></td>
+                <td><img src="images/ikoner/citat.png" alt="Kommunalvalg - Citat" title="Citat"></td>
                 <td>Alt hvad der kommer ud af hendes mund, bør censureres</td>
               </tr>
             </table>
@@ -87,7 +87,7 @@
         <div class="hero-content-text">
           <table>
             <tr>
-              <td><img class="icon-large" src="images/ikoner/politik.png" alt="Kommunalvalg - Politik"></td>
+              <td><img class="icon-large" src="images/ikoner/politik.png" alt="Kommunalvalg - Politik" title="Politiske holdninger"></td>
               <td>Jeg synes det er forkert at blive tvunget til at betale licens. Højere friværdi før vi skal betale skat - det giver ingen mening at betale skat af SU. Mere fokus på at forbedre byen og gaderne.</td>
             </tr>
           </table>
--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,8 @@
 <footer>
 <div class="grid">
-  <p>&copy; <?php echo date("Y"); ?> Copyright.</p>
+  <p>&copy; <?php echo date("Y"); ?> Copyright.
+  <a href="#"><img src="images/ikoner/facebook.png" alt="Facebook Icon" title="Follow us on Facebook!"></a>
+  <a href="#"><img src="images/ikoner/instagram.png" alt="Facebook Icon" title="Follow us on instagram"></a>
+  </p>
 </div>
 </footer>
